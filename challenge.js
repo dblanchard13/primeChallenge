17f678b01a2dab64970a678b8bb3eefeb3e51f79
dblanchard13 <davidb.prof@gmail.com>
2015-04-30 14:17:26 -0700

    Correct running instructions comments

--- challenge.js
+++ challenge.js
@@ -1,28 +1,18 @@
 // Objective:
 // Write a program that prints out a multiplication table of the first 10 prime numbers.
 
-// The program must run from the command line and print one table to STDOUT.
-  // The program can be run from the command line by running the following: 
-    // node challenge.js
-  // The tests can be run from the command line by running the following:
-    // npm test
-  // Since my solution is in JavaScript, the table is printed out as a series of console.logs
+// The program can be run from the command line by navigating to the the root directory and running the following: 
+  // npm start
 
-// The first row and column of the table should have the 10 primes, with each cell 
-// containing the product of the primes for the corresponding row and column.
+// The tests can be run from the command line by navigating to the the root directory and running the following:
+  // npm test
+  
+// Since my solution is in JavaScript, the table is printed out as a series of console.logs
 
-// Notes:
-// Consider complexity. How fast does your code run? How does it scale?
-
-// Consider cases where we want N primes.
-
-// Do not use the Prime class from stdlib (write your own code).
-
-// Write tests. Try to demonstrate TDD/BDD.
 
 var primeGetter = function(n){
   // n is the number of primes to get
-  // If no n is passed into the function, it defaults to 10
+    // If no n is passed into the function, it defaults to 10
   n = n || 10;
   var primes = [];
   // Initialize x as the first prime number, 2
